58e4260a1b8b6e15a95b1a64db7a057ccac196ad
Macbook <natalynoele@gmail.com>
2023-09-20 11:54:15 +0300

    generics

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface T {
+  title: string;
+}
 
-  }
+class Component {
+  constructor(public props: T) {}
 }
 
 class Page extends Component {
-  pageInfo () {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
